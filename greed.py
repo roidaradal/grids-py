f91cd2b804e3a957f00a36312fc45119fb696daf
roidaradal <johnroy.daradal@goodkredit.com>
2025-12-31 16:36:33 +0800

    New Greed

---
+++ greed.py
@@ -0,0 +1,32 @@
+import random
+from grid import * 
+
+GREED_CURSOR = -1
+
+# Create new Greed grid 
+def new_Greed() -> IntGrid:
+    # Make blank IntGrid 
+    rows, cols = 22, 79
+    grid = new_IntGrid(rows, cols)
+    # Randomize grid digits 
+    per_digit = 193 
+    digits: list[int] = [GREED_CURSOR]
+    for i in range(1, 10):
+        digits.extend([i] * per_digit)
+    random.shuffle(digits)
+    for idx, digit in enumerate(digits):
+        row, col = index_to_coords(idx, cols)
+        grid[row][col] = digit 
+    return grid
+
+# Display Greed grid 
+def display_Greed(grid: IntGrid):
+    table: dict[int, str] = {
+        0: "%2s" % ".", # empty cell
+        GREED_CURSOR: "%2s" % "@", # cursor cell
+    }
+    def to_string(cell: int) -> str:
+        if cell not in table:
+            return "%2d" % cell 
+        return table[cell]
+    display_grid(grid, to_string)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from g2048 import *
+from greed import *
 
 # Game constants
 GAME_2048 = "2048"
@@ -7,10 +8,13 @@ GAME_GREED = "greed"
 GAME_MINES = "mines"
 
 def main():
-    option = GAME_2048 
+    option = GAME_GREED
     if option == GAME_2048:
         grid = new_2048()
         display_2048(grid)
+    elif option == GAME_GREED:
+        grid = new_Greed()
+        display_Greed(grid)
     else:
         print('Unknown option:', option)
 
